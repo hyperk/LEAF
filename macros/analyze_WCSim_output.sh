4c4a04b2a8d9f6c77f9ccd5f42a7e53858a473ed
bquilain <bquilain@sukap002v301.km.icrr.u-tokyo.ac.jp>
2020-11-14 02:49:11 +0900

    Modified dummy shell file

---
+++ macros/analyze_WCSim_output.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh +x
+./AnalyzeWSHierarchy -h -f /disk01/usr5/bquilain/WCSimData/HK_2020/wcsim_hkhybridmpmt10pc14374100Hz_4.2kHzBL_e500_center_nominal_closewall_0hitstrigger_20000.root -s 0 -e 1000 -o /disk01/usr5/bquilain/test.root > /disk01/usr5/bquilain/tt.txt
