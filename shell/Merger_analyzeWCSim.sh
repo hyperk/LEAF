efd25e43c2a52e62cf0098048e55223d8046cd68
Shota Izumiyama <izumiyama@hep.phys.titech.ac.jp>
2020-12-15 17:05:18 +0900

    Modified sample shell scripts, but they may need more explantions how to use
    	modified:   shell/Launcher.sh
    	modified:   shell/Launcher_analyzeWCSim.sh
    	modified:   shell/Merger_analyzeWCSim.sh
    	modified:   shell/generateShell.c
    	deleted:    shell/generateShell.c~
    	modified:   shell/generateShell_analyzeWCSim.c

--- shell/Merger_analyzeWCSim.sh
+++ shell/Merger_analyzeWCSim.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh +x
-for e in 10;do
-#for e in 3 4 5 6 8 10 15;do
+# Sampele shell scripts showing how to process in queue system in sukap
+# This assums to be used with Launcher_analyzeWCSim.sh and generateShell_analyzeWCSim.c
+# 
+# This just merge generated root-files by them
+
+for e in 3 4 5 6 8 10 15;do
     hadd -f /disk01/usr5/bquilain/Analyze_hkhybridmpmt10pc14374100Hz_4.2kHzbl_${e}MeV.root /disk01/usr5/bquilain/Analyze_hkhybridmpmt10pc14374100Hz_4.2kHzbl_${e}MeV_tmp*.root
 done
--- shell/generateShell.c
+++ shell/generateShell.c
@@ -1,3 +1,6 @@
+// This is a sample script to be executed in root
+// Usage: root generateShell.c
+
 {
   char * ShellName = new char[256];
   ofstream ShellFile(ShellName);
@@ -7,27 +10,24 @@
   double dr = 0;
   bool mPMT = true;
   double dr_mPMT = 0;
-    
+
   int nevents=50000;
   int neventsPerFile=500;
-  //for(int nrj=10;nrj<11;nrj++){
-  for(int nrj=3;nrj<16;nrj++){
-    //sprintf(infilename,"/disk01/usr5/bquilain/wcsim_hkhybridmpmt10pc14374100Hz_4.2kHzBL_e%d_center_nominal_closewall_0hitstrigger_100000.root",nrj);
-    sprintf(infilename,"/disk01/usr5/bquilain/wcsim_hkhybridmpmt10pc14374100Hz_4.2kHzBL_e%d_center_nominal_fulltank_0hitstrigger_100000.root",nrj);
+  for(int nrj=3;nrj<16;nrj++){ // nrj -> energy
+    sprintf(infilename,"/disk01/usr5/bquilain/wcsim_hkhybridmpmt10pc14374100Hz_4.2kHzBL_e%d_center_nominal_fulltank_0hitstrigger_100000.root",nrj); // WCSim file to be analyzed
 
     for(int startBin=0;startBin<nevents;startBin+=neventsPerFile){
-      sprintf(ShellName,"%s/jobs/fitLE_e%d_start%d.sh",LEAFDIR,nrj,startBin);
-      sprintf(outfilename,"/disk01/usr5/bquilain/LEAFv2_hkhybridmpmt10pc14374100Hz_4.2kHzbl_%dMeV_tmp%d.root",nrj,startBin);
-    
+      sprintf(ShellName,"%s/jobs/fitLE_e%d_start%d.sh",LEAFDIR,nrj,startBin); // shell file to be submitted to queue system
+      sprintf(outfilename,"%s/jobs/root/hkhybridmpmt10pc14374100Hz_4.2kHzbl_%dMeV_tmp%d.root",LEAFDIR, nrj,startBin); // outpu t file name of process 
+
       ofstream ShellFile(ShellName);
       if(ShellFile){
-	ShellFile<<"#!/bin/bash"<<endl;
-	ShellFile<<Form("rm -f %s",outfilename)<<endl;
-	ShellFile<<Form("source /disk01/usr5/bquilain/LEAF/LEAF_v1.5/LEAF/RunAtStart_sukap.sh")<<endl;
-	ShellFile<<Form("%s/app/analysis %s %s %d %d %2.2f %s",LEAFDIR,infilename,outfilename,startBin,startBin+neventsPerFile,dr,mPMT?Form("%2.2f",dr_mPMT),"")<<endl;
-	//ShellFile<<Form("/home/bquilain/nuPRISM/BenjaminAnalysis/FitVertexLE_v2 -f %s -o %s -s %d -e %d",infilename,outfilename,startBin,startBin+neventsPerFile)<<endl;
-	//ShellFile<<Form("%s/app/FitVertexLE -f %s -o %s -s %d -e %d",LEAFDIR,infilename2,outfilename2,startBin,startBin+neventsPerFile)<<endl;
+        ShellFile<<"#!/bin/bash"<<endl;
+        ShellFile<<Form("rm -f %s",outfilename)<<endl;
+        ShellFile<<Form("source %s/RunAtStart_sukap.sh", LEAFDIR)<<endl;
+        ShellFile<<Form("%s/example/analysis %s %s %d %d %2.2f %s",LEAFDIR,infilename,outfilename,startBin,startBin+neventsPerFile,dr,mPMT?Form("%2.2f",dr_mPMT),"")<<endl;
       }
+      ShellFile.close();
     }
   }
 }
