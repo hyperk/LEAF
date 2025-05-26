a67bbc88da0090ab0bfd54ec76ec76eb14614143
nmoreau <moreau.nicolas7579@gmail.com>
2025-05-26 06:42:58 +0200

    Goodness of Fit maths

--- leaf/LeafUtility.cc
+++ leaf/LeafUtility.cc
@@ -1,11 +1,12 @@
 #include "LeafUtility.hh"
 
-bool ContainsTrueVtx(std::vector<std::vector<double>>* tRecoVtxPos, std::vector<double> fTrueVtxPos)
+bool ContainsTrueVtx(std::vector<VtxCandidate>* candidates, std::vector<double> fTrueVtxPos)
 {
-	for (unsigned int i = 0; i < tRecoVtxPos->size(); i++)
+	for (unsigned int i = 0; i < candidates->size(); i++)
 	{
-		double distance = Distance3D((*tRecoVtxPos)[i], fTrueVtxPos);
-		if (distance <= fSearchVtxStep)
+		std::vector<double> vtx = {(*candidates)[i].X, (*candidates)[i].Y, (*candidates)[i].Z, (*candidates)[i].T};
+		double distance = Distance3D(vtx, fTrueVtxPos);
+		if (distance <= fSearchVtxStep) //? should we take step / 2 ?
 		{
 			return true;
 		}
@@ -13,16 +14,27 @@ bool ContainsTrueVtx(std::vector<std::vector<double>>* tRecoVtxPos, std::vector<
 	return false;
 }
 
-//* to know if the predicted vertex and the true vertex belong to the same candidate
-//? doesn't work because the radius overlap between candidates
-bool CorrectCandidate(std::vector<std::vector<double>>* candidates, std::vector<double> point, std::vector<double> fTrueVtxPos)
+bool ContainsTrueDir(std::vector<DirectionCandidate>* candidates, std::vector<double> fTrueDir)
 {
-	for(unsigned int i = 0; i < candidates->size(); i++)
+	for (unsigned int i = 0; i < candidates->size(); i++)
 	{
-		if (Distance3D((*candidates)[i], fTrueVtxPos) <= fSearchVtxStep && Distance3D((*candidates)[i], point) <= fSearchVtxStep)
+		std::vector<double> truePolarDir = CartesianToPolarNorm(fTrueDir);
+		if(abs((*candidates)[i].theta - truePolarDir[0]) < theta_step && abs((*candidates)[i].phi - truePolarDir[1]) < phi_step)
 		{
 			return true;
 		}
+	}
+	return false;
+}
+
+
+//* to know if the predicted vertex and the true vertex belong to the same candidate
+//? doesn't work because the radius overlap between candidates
+bool CanidatesMatch(std::vector<std::vector<double>>* candidates, std::vector<double> point, std::vector<double> fTrueVtxPos)
+{
+	for(unsigned int i = 0; i < candidates->size(); i++)
+	{
+		if (Distance3D((*candidates)[i], fTrueVtxPos) <= fSearchVtxStep && Distance3D((*candidates)[i], point) <= fSearchVtxStep) return true;
 	}
 	return false;
 }
@@ -32,4 +44,225 @@ double Distance3D(std::vector<double> point1, std::vector<double> point2)
 	return sqrt(pow(point1[0] - point2[0], 2) + 
 				pow(point1[1] - point2[1], 2) + 
 				pow(point1[2] - point2[2], 2));
+}
+
+void Normalize(double a[3])
+{
+	double l;
+	l=sqrt(a[0]*a[0]+a[1]*a[1]+a[2]*a[2]);
+	for(int j=0; j<3; j++){
+		a[j]/=l;
+	}
+}
+
+void Normalize(std::vector<double>& vector)
+{
+	if (vector.size() < 3) throw std::invalid_argument("Vector must be of size at least 3 to be normalized");
+	double length = sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
+	if (length == 0) throw std::invalid_argument("Cannot normalize a zero-length vector.");
+	for (int i = 0; i < 3; ++i) vector[i] /= length;
+}
+
+double calculateDistance(const std::vector<double>& A, const std::vector<double>& B) 
+{
+    return sqrt(pow(A[0] - B[0], 2) + pow(A[1] - B[1], 2) + pow(A[2] - B[2], 2));
+}
+
+double dot(const std::vector<double>& A, const std::vector<double>& B)
+{
+	if (A.size() != B.size()) throw std::invalid_argument("Vectors must be of the same size for dot product.");
+	double result = 0;
+	for (size_t i = 0; i < A.size(); ++i) result += A[i] * B[i];
+	return result;
+}
+
+std::vector<double> PolarToCartesianNorm(std::vector<double> polarVector)
+{
+	double theta = polarVector[0];
+	double phi = polarVector[1];
+	std::vector<double> cartVector = std::vector<double>(3);
+	cartVector[0] = sin(theta) * cos(phi);
+	cartVector[1] = sin(theta) * sin(phi);
+	cartVector[2] = cos(theta);
+	Normalize(cartVector);
+	return cartVector;
+}
+
+std::vector<double> CartesianToPolarNorm(const std::vector<double>& cartVector)
+{
+    std::vector<double> polarVector(2);
+    double x = cartVector[0];
+    double y = cartVector[1];
+    double z = cartVector[2];
+
+    polarVector[0] = acos(z);              // theta
+    polarVector[1] = atan2(y, x);          // phi
+
+    return polarVector;
+}
+
+double EstimateBandeWidth(const std::vector<double>& residuals) 
+{
+	double sum = std::accumulate(residuals.begin(), residuals.end(), 0.0);
+	double mean = sum / residuals.size();
+
+	double sq_sum = std::inner_product(residuals.begin(), residuals.end(), residuals.begin(), 0.0);
+	double variance = sq_sum / residuals.size() - mean * mean;
+	double stddev = std::sqrt(variance);
+	return 1.06 * stddev * std::pow(residuals.size(), -1.0 / 5.0);
+}
+
+double GaussianKernel(double x, double bandwidth) 
+{
+    return std::exp(-0.5 * x * x / (bandwidth * bandwidth)) / (bandwidth * std::sqrt(2.0 * M_PI));
+}
+
+double KDE_Estimate(const std::vector<double>& residuals, double t_i, double bandwidth) 
+{
+    double sum = 0.0;
+    for (const double& t_j : residuals) sum += GaussianKernel(t_i - t_j, bandwidth);
+    return sum / residuals.size();
+}
+
+double ComputeResidualTime(std::vector<double> vertexPos, double originTime, Hit lHit)
+{
+	int iPMT = lHit.PMT;
+	double HitT = (fTimeCorrection + lHit.T) / TimeDelta::ns;
+	PMTInfo lPMTInfo = (*fPMTList)[iPMT];
+	double distance = Astro_GetDistance(lPMTInfo.Position, vertexPos);
+
+	double tof = distance / fLightSpeed;
+	return HitT - tof - originTime;
+}
+
+VtxCandidate ComputeCandidateSNR(const std::vector<double>& vertex, double lowerLimit, double upperLimit)
+{
+	VtxCandidate out;
+
+	// Copy the vertex info
+	out.X = vertex[0];
+	out.Y = vertex[1];
+	out.Z = vertex[2];
+	out.T = vertex[3];
+	out.NLL = 0.0; 
+
+	// We'll do a simple approach: count how many hits are in [lower,upper]
+	// for each PMT type, then subtract the dark rate.
+
+	int nHitsTotal = fHitCollection->Size();
+	int inTimeBnL = 0;
+	int inTimemPMT = 0;
+
+	for(int iHit = 0; iHit < nHitsTotal; iHit++) {
+		const Hit& hit = fHitCollection->At(iHit);
+		int iPMT = hit.PMT;
+		int pmtType = Astro_GetPMTType(iPMT); // 0=BnL, 1=mPMT
+
+		double hitTime = (fTimeCorrection + hit.T) / TimeDelta::ns;
+		PMTInfo pInfo = (*fPMTList)[iPMT];
+		double distance = Astro_GetDistance(pInfo.Position, vertex);
+		double tof = distance / fLightSpeed;
+		double residual = hitTime - tof - vertex[3];
+		//std::cout << "residual: " << residual << ", " << "lowerlimit and upperlimit " << lowerLimit << ", " << upperLimit << std::endl; 
+		if(residual >= lowerLimit && residual <= upperLimit)
+		{
+			if(pmtType == 0) {
+			// BnL
+			inTimeBnL++;
+			} else {
+			// mPMT
+			inTimemPMT++;	
+			}
+		}
+	}
+
+	double dt = (upperLimit - lowerLimit);
+
+	// BnL:
+	double darkRateBnL = fTimeWindowSizeFull * fDarkRate_ns[NormalPMT];
+	// average dark over dt:
+	double darkInWindowBnL = (dt / double(fTimeWindowSizeFull)) * darkRateBnL;
+	double signalBnL = std::max(double(inTimeBnL) - darkInWindowBnL, 0.0);
+	//std::cout << "inTimeBnL - darkInWindowBnL : " << inTimeBnL << "-" << darkInWindowBnL << std::endl;
+	out.SNR = (darkInWindowBnL > 1e-9) ? (signalBnL / darkInWindowBnL) : 999.0;
+
+	return out;
+}
+
+void VectorVertexPMT(std::vector<double> vertex, int iPMT, double *dAngles)
+{
+	// Guillaume 2020/05/20:
+	// mPMT referencial is computed once for all PMT in LoadPMTInfo() and MakeMPMTReferencial(iPMT)
+	// Guillaume 2020/11/20:
+	// Reference is moved to outside LEAF (HKManager or hk-AstroAnalysis)
+
+	PMTInfo lPMTInfo = (*fPMTList)[iPMT];
+	int iPMTTop = lPMTInfo.mPMT_RefTube;
+	PMTInfo lPMTInfoTop = (*fPMTList)[iPMTTop];
+
+	// 5. Now we have our referential, we should just calculate the angles of the PMT to vertex position vector in this referential.
+	// a. calculate the PMT to vertex position vector.
+
+	double dVtx_PMTRef[3];
+
+	dVtx_PMTRef[0] = vertex[0] - lPMTInfoTop.Position[0];
+	dVtx_PMTRef[1] = vertex[1] - lPMTInfoTop.Position[1];
+	dVtx_PMTRef[2] = vertex[2] - lPMTInfoTop.Position[2];
+
+	double dLengthVtx = Astro_GetLength(dVtx_PMTRef);
+	GeoTools::Normalize(dVtx_PMTRef);
+
+	if (VERBOSE >= 3)
+	{
+		std::cout << "Vertex position = " << vertex[0] << ", " << vertex[1] << ", " << vertex[2] << std::endl;
+		std::cout << "Vertex position from PMT = " << dVtx_PMTRef[0] << ", " << dVtx_PMTRef[1] << ", " << dVtx_PMTRef[2] << std::endl;
+	}
+
+	// b. Then extract Theta and Phi:
+	double dCosTheta = Astro_GetScalarProd(dVtx_PMTRef, lPMTInfo.mPMT_RefZ);
+	double dTheta = TMath::ACos(dCosTheta);
+
+	double dPhi = 0.;
+
+	if ((*fPMTList)[iPMT].mPMT_TubeNum == HKAA::kmPMT_TopID)
+	{
+		// Phi is not defined in that case..
+	}
+	else
+	{
+		// We know x=cosPhi x sinTheta and y=sinPhi x sinTheta
+		double dX = Astro_GetScalarProd(dVtx_PMTRef, lPMTInfo.mPMT_RefX);
+		double dY = Astro_GetScalarProd(dVtx_PMTRef, lPMTInfo.mPMT_RefY);
+		double dTanPhi = dY / dX;
+		dPhi = TMath::ATan(dTanPhi);
+
+		// tan is symetric from -pi/2 to +pi/2
+		if (dX == 0)
+		{
+			if (dY < 0)
+				dPhi = -TMath::Pi() / 2.;
+			else
+				dPhi = TMath::Pi() / 2.;
+		}
+		if (dX < 0)
+			dPhi += TMath::Pi(); // With this, angle become defines between -pi/2 to -pi/2 +2pi.
+
+		// We wish to bring this from 0 to 2pi:
+		if (dPhi < 0)
+			dPhi += 2 * TMath::Pi();
+
+		// Actually, we have a symmetry in Phi between [0,pi] and [pi,2pi]. So, we will just define Phi in [0,pi] modulo pi
+		if (dPhi > TMath::Pi())
+			dPhi = TMath::Pi() - (dPhi - TMath::Pi());
+	}
+
+	dAngles[0] = dPhi * 180. / TMath::Pi();
+	dAngles[1] = dTheta * 180. / TMath::Pi();
+	dAngles[2] = dLengthVtx;
+	dAngles[3] = 1; // calculateWeight(dLengthVtx,PMTradius[pmtType],Theta,verbose);
+
+	if (VERBOSE >= 3)
+	{
+		std::cout << "Angles Phi = " << dAngles[0] << ", Theta = " << dAngles[1] << std::endl;
+	}
 }
--- leaf/LeafUtility.hh
+++ leaf/LeafUtility.hh
@@ -4,11 +4,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
+#include <numeric>
 
 //* DataModel informations
 #include "Geometry.h"
 #include "HitCollection.h"
 #include "LeafConfig.hh"
+#include "LeafInputs.hh"
+#include "LeafDefinitions.hh"
 
 inline bool SortOutputVector ( const std::vector<double>& v1, const std::vector<double>& v2 ) 
 { 
@@ -17,6 +21,35 @@ inline bool SortOutputVector ( const std::vector<double>& v1, const std::vector<
 
 double Distance3D(std::vector<double> point1, std::vector<double> point2);
 		
-bool CorrectCandidate(std::vector<std::vector<double>>* candidates, std::vector<double> point, std::vector<double> fTrueVtxPos);
+void Normalize(double a[3]);
 
-bool ContainsTrueVtx(std::vector<std::vector<double>>* tRecoVtxPos, std::vector<double> fTrueVtxPos);
+void Normalize(std::vector<double>& vector);
+
+double dot(const std::vector<double>& A, const std::vector<double>& B);
+
+double calculateDistance(const std::vector<double>& A, const std::vector<double>& B);
+
+std::vector<double> PolarToCartesianNorm(std::vector<double> polarVector);
+
+std::vector<double> CartesianToPolarNorm(const std::vector<double>& cartVector);
+
+bool CanidatesMatch(std::vector<std::vector<double>>* candidates, std::vector<double> point, std::vector<double> fTrueVtxPos);
+
+bool ContainsTrueVtx(std::vector<VtxCandidate>* candidates, std::vector<double> fTrueVtxPos);
+
+bool ContainsTrueDir(std::vector<DirectionCandidate>* candidates, std::vector<double> fTrueDir);
+
+//* Estimate P_data(t_i) using KDE
+double KDE_Estimate(const std::vector<double>& residuals, double t_i, double bandwidth);
+
+double EstimateBandeWidth(const std::vector<double>& residuals);
+
+double ComputeResidualTime(std::vector<double> vertexPos, double originTime, Hit lHitt);
+
+//* Gaussian kernel function
+double GaussianKernel(double x, double bandwidth);
+
+//* Signal noise ratio
+VtxCandidate ComputeCandidateSNR(const std::vector<double>& vertex, double lowerLimit, double upperLimit);
+
+void VectorVertexPMT(std::vector<double> vertex, int iPMT, double* dAngles );
